2cbac35f59bd92615d8b0bcd2747bb179b5c4f44
nerone709 <napolitanogiuseppe709@gmail.com>
2023-04-19 20:00:58 +0200

    first commit

---
+++ Assets/Bird_Script.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird_Script : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float flap;
+    public LogicScript logic;
+    public bool isAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true && isAlive)
+        {
+            myRigidBody.velocity = Vector2.up * flap;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        isAlive = false;
+    }
+}
